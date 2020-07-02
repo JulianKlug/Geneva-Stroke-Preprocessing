16e0f1f170da9ed0fe2175ca6d8c4336118c42ae
monsieurwave <tensu.wave@gmail.com>
2020-07-03 01:23:30 +0200

    Added a new dataset histogram display function in a single image

--- gsprep/visual_tools/dataset_histograms.py
+++ gsprep/visual_tools/dataset_histograms.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 def plot_per_sample_histograms(data, ids = None , log = False , bins = 50, title = None, show=False, figsize=(10,10)):
     """
@@ -29,3 +31,62 @@ def plot_per_sample_histograms(data, ids = None , log = False , bins = 50, title
     if show:
         plt.show()
     return fig
+
+def plot_dataset_histogram(data, num_positions=100, log=False, mask_zero=False, dataset_label=None, alpha=0.05, color=None,
+                           axis=None, figsize=(10,10)):
+    """
+    Plot an histogram for a dataset
+    :param data: (n_samples, x, y, z)
+    :param num_positions: number of position sampled in kernel
+    :param log: display in log scale
+    :param mask_zero: mask zeros out of data
+    :param dataset_label: label for all values in this dataset
+    :param alpha:
+    :param color:
+    :param axis:
+    :param figsize:
+    :return:
+    """
+    if axis is None:
+        fig = plt.figure(figsize=figsize)
+        axis = fig.add_subplot(1, 1, 1)
+    for subj_idx in range(data.shape[0]):
+        label = None
+        if dataset_label is not None and subj_idx == 0:
+            label = dataset_label
+        plot_histogram(data[subj_idx], num_positions=num_positions, log=log, mask_zero=mask_zero, label=label,
+                       alpha=alpha, color=color, axis=axis)
+
+
+def plot_histogram(data, num_positions=100, log=False, mask_zero=False, label=None, alpha=0.05, color=None, axis=None,
+                   figsize=(10,10)):
+    """
+    Plot an histogram for this data sample
+    :param data: (n_samples, x, y, z)
+    :param num_positions: number of position sampled in kernel
+    :param log: display in log scale
+    :param mask_zero: mask zeros out of data
+    :param label:
+    :param alpha:
+    :param color:
+    :param axis:
+    :param figsize:
+    :return:
+    """
+    if axis is None:
+        fig = plt.figure(figsize=figsize)
+        axis = fig.add_subplot(1, 1, 1)
+
+    values = data.ravel()
+    if mask_zero:
+        values = values[np.argwhere(values != 0)].ravel()
+    kernel = stats.gaussian_kde(values)
+    positions = np.linspace(values.min(), values.max(), num=num_positions)
+    histogram = kernel(positions)
+    kwargs = dict(linewidth=1, color='black' if color is None else color, alpha=alpha)
+    if label is not None:
+        kwargs['label'] = label
+    axis.plot(positions, histogram, **kwargs)
+    if log:
+        axis.set_yscale("log")
+
